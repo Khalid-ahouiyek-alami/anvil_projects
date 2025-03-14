6aafeb968ea0b07f066bddcc5e2798d2e4f07c85
khalid1alami1@gmail.com <khalid1alami1@gmail.com>
2025-03-14 14:45:48 +0000

    Edited MovieList, settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -29,23 +29,17 @@ def delete_movie(movie):
 
  
 @anvil.server.callable
-def decode(enc):
-    key = anvil.secrets.get_secret('lickey')
-    dec = []
+def decode(enc, key):
+  dec = []
+  enc2 = base64.urlsafe_b64decode(enc)
 
-    enc2 = base64.urlsafe_b64decode(enc)
+  for i in range(len(enc2)):
+      key_c = key[i % len(key)]
+      dec_c = chr((256 + enc2[i] - ord(key_c)) % 256) 
+      dec.append(dec_c)
+  decB64 = "".join(dec)
 
-    for i in range(len(enc2)):
-
-        key_c = key[i % len(key)]
-
-        dec_c = chr((256 + enc2[i] - ord(key_c)) % 256)
-
-        dec.append(dec_c)
-
-    decB64 = "".join(dec)
-
-    return decB64
+  return decB64
 
  
 
@@ -55,14 +49,38 @@ def decode(enc):
 
 def get_userprofiles():
   licfile = anvil.secrets.get_secret('licfile')
-  declicense = decode(licfile)
+  lickey = anvil.secrets.get_secret('lickey')
+  deckey = anvil.secrets.get_secret('deckey')
+  declicense = decode(licfile, lickey)
   licwords = str(declicense).split('[---]')
   sgaccount = licwords[7]
   dbconfigkey = licwords[17]
   dbconfigpwd = licwords[26]
   client = Cloudant(dbconfigkey, dbconfigpwd, account=sgaccount)
   client.connect()
+  
   my_db = client['userprofiles']
   docs_found = my_db.all_docs(include_docs=True)
+ 
+  dataregels = docs_found['rows']
+  numlen = len(dataregels)
 
-  return docs_found['rows']
+  for idx in range (0, numlen):
+    if 'doc' in dataregels[idx]:
+      user_data = dataregels[idx]['doc']
+      if 'FIRSTNAME' in user_data:
+        enc_firstname = user_data['FIRSTNAME']
+        enc_surname = user_data['SURNAME']
+        enc_email = user_data['EMAIL']
+        firstname = decode(enc_firstname, deckey)
+        surname = decode(enc_surname, deckey)
+        email = decode(enc_email, deckey)
+
+        print(f"voornaam: {firstname} - achternaam: {surname} - email: {email}")
+      
+  
+  
+  
+  return docs_found['rows'] 
+
+  
